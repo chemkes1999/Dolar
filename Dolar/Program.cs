7f16e7b151224d3ea01a7319d1548d6743929ce4
Carlos Hemkes <c.hemkes1999@gmail.com>
2023-08-07 08:56:51 -0600

    reform

--- Dolar/Program.cs
+++ Dolar/Program.cs
@@ -1,52 +1,48 @@
-﻿using System;
-using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
-using iTextSharp.text;
+﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
 
-class Program
+internal class Program
 {
-    static async Task Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        string apiKey = "apikey";
-        string baseCurrency = "USD";
-        string apiUrl = $"https://v6.exchangerate-api.com/v6/{apiKey}/latest/{baseCurrency}";
+        var apiKey = "apikey";
+        var baseCurrency = "USD";
+        var apiUrl = $"https://v6.exchangerate-api.com/v6/{apiKey}/latest/{baseCurrency}";
         try
         {
-                using (var httpClient = new HttpClient())
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
                 {
-                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    // Aquí tienes la respuesta en formato JSON. Puedes procesarla según tus necesidades.
+                    Console.WriteLine(responseContent);
 
-                    if (response.IsSuccessStatusCode)
+                    // Guardar la respuesta como archivo PDF
+                    var pdfFileName = "exchange_rates.pdf";
+                    using (var fs = new FileStream(pdfFileName, FileMode.Create))
                     {
-                        string responseContent = await response.Content.ReadAsStringAsync();
-                        // Aquí tienes la respuesta en formato JSON. Puedes procesarla según tus necesidades.
-                        Console.WriteLine(responseContent);
+                        var document = new Document();
+                        PdfWriter.GetInstance(document, fs);
 
-                        // Guardar la respuesta como archivo PDF
-                        string pdfFileName = "exchange_rates.pdf";
-                        using (FileStream fs = new FileStream(pdfFileName, FileMode.Create))
-                        {
-                            Document document = new Document();
-                            PdfWriter.GetInstance(document, fs);
-
-                            document.Open();
-                            document.Add(new Paragraph(responseContent));
-                            document.Close();
-                        }
-
-                        Console.WriteLine($"Los datos se han guardado correctamente en {pdfFileName}.");
+                        document.Open();
+                        document.Add(new Paragraph(responseContent));
+                        document.Close();
                     }
-                    else
-                    {
-                        Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
-                    }
+
+                    Console.WriteLine($"Los datos se han guardado correctamente en {pdfFileName}.");
                 }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
         }
         catch (Exception ex)
         {
-                Console.WriteLine($"Error: {ex.Message}");
+            Console.WriteLine($"Error: {ex.Message}");
         }
     }
 }
